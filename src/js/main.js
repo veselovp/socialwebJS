fb014e0669f3968ec8651235e8baf13d540f0905
Pavel Vslv <114096039+veselovp@users.noreply.github.com>
2023-04-26 17:17:54 +0100

    youtubeplayer

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
 import Slider from './modules/slider'
+import VideoP from './modules/videoplayer'
 
 window.addEventListener('DOMContentLoaded', () => {
   const slider = new Slider('.page', '.next')
   slider.render()
+
+  const player = new VideoP('.showup .play', '.overlay')
+  player.init()
 })
