1bb46f1a06ad5e483016ba7437e68fca65e3dea6
Pavel Vslv <114096039+veselovp@users.noreply.github.com>
2023-04-26 15:04:42 +0100

    add pop um mkister johnoss

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -15,6 +15,20 @@ export default class Slider {
       this.slideIndex = this.slides.length
     }
 
+    // pojavlajetsja blok s zaderkoj v 3 sekundi
+    try {
+      this.hanson.style.opacity = '0'
+      if (n === 3) {
+        this.hanson.classList.add('animated')
+        setTimeout(() => {
+          this.hanson.style.opacity = '1'
+          this.hanson.classList.add('slideInUp')
+        }, 3000)
+      } else {
+        this.hanson.classList.remove('slideInUp')
+      }
+    } catch (e) {}
+
     Array.from(this.slides).forEach((slide) => {
       slide.style.display = 'none'
     })
@@ -27,6 +41,12 @@ export default class Slider {
   }
 
   render() {
+    // pojavlajetsja blok s zaderkoj v 3 sekundi
+    // try esli .hanson nebilo to chtobi vikinutj eroor i esli tru to pojavitsja novoje svojstvo hanson
+    try {
+      this.hanson = document.querySelector('.hanson')
+    } catch (e) {}
+
     this.btns.forEach((item) => {
       item.addEventListener('click', () => {
         this.plusSlides(1)
